fe701ba3181b9847f0166b99e7a19001ae8396bf
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-07-15 11:20:16 -0400

    added variables for message prompts

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,8 @@ function generatePassword() {
   var length = parseInt(prompt("Welcome! Please enter at least 8 characters and no more than 128 characters to generate password:"));
   var uppercase = confirm("Would you like uppercase letters? Click OK to continue");
   var lowercase = confirm("Would you like lowercase letters? Click OK to continue");
+  var symbols = confirm("Would you like symbols? Click OK to continue");
+  var numbers = confirm("Would you like numbers? Click OK to continue");
 
   //1. Prompt user for PW criteria
   //   a. PW length at least 8 characters and no more than 128 characters
