1ee2cdd25de358b4233f4487155d571ceeb857b9
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-07-15 15:28:52 -0400

    added another if statement

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,17 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var length = parseInt(prompt("Welcome! Please choose a length of at least 8 characters and no more than 128 characters:"));
-  if (isNaN(length) || length < 8 || length > 128) {
-    alert("Password length should be a number between 8 and 128 characters. Please try again:");
+  if (length < 8 || length > 128) {
+    alert("Password length should be between 8 and 128 characters. Please try again:");
     return generatePassword();
-  } else {
+  } 
+  
+  if (isNaN(length)) {
+    alert("Please enter a number. Try using 'Generate Password' again:")
+    return null;
+  }
+  
+  else {
     var useUppercase = confirm("Do you want to include uppercase letters?");
     var useLowercase = confirm("Do you want to include lowercase letters?");
     var useNumbers = confirm("Do you want to include numbers?");
@@ -18,7 +25,7 @@ function generatePassword() {
 
   //1. Prompt user for PW criteria
   //   a. PW length at least 8 characters and no more than 128 characters
-  //   b. Lowercase, uppercase, numbers, special characters
+  //   b. Uppercase, lowercase, numbers, special characters
   //2. Validate the input.
   //3. Generate PW based on criteria.
 
