0db98e6656258920e2c0bb382e0d3f4c6a9f648c
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-07-15 10:53:08 -0400

    added message prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var length = parseInt(prompt("Please enter password. Must be at least 8 characters and no more than 128 characters:"));
+
+  //1. Prompt user for PW criteria
+  //   a. PW length at least 8 characters and no more than 128 characters
+  //   b. Lowercase, uppercase, numbers, special characters
+  //2. Validate the input.
+  //3. Generate PW based on criteria.
+
+  
+  //4. Display PW to the page.
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
